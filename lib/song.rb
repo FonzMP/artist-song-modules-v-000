350d9230e7c43a46683c6f034a8ec83de7d86a6e
Alex Wilson <fonzmp@gmail.com>
2018-05-21 17:29:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,17 +10,9 @@ class Song
 
   @@songs = []
 
-  # def initialize
-  #   @@songs << self
-  # end
-
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@songs

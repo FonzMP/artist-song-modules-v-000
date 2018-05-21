f2a25ffd130b3f5219f77b724e27983a1106765d
Alex Wilson <fonzmp@gmail.com>
2018-05-21 17:22:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
 
   extend Memorable::ClassMethods, Findable::ClassMethods
+  include Paramble::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
@@ -26,4 +27,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
 end
